688cbc9d23ea2c84866b73109578571b19f0e639
Cartifon <ciceroartifon@gmail.com>
2015-11-15 22:15:18 -0200

    Adicionando uma lógica para pegar dados de páginas html, pra tnetar pegar automaticamente as palavras de lá depois

--- src/org/com/mots/main/Main.java
+++ src/org/com/mots/main/Main.java
@@ -2,17 +2,14 @@ package org.com.mots.main;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
-import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.com.mots.util.FileUtil;
+import org.com.mots.util.HTMLsUtils;
 
 public class Main {
 
@@ -20,21 +17,35 @@ public class Main {
 	 * File file = new File("/commons/io/project.properties"); List lines =
 	 * FileUtils.readLines(file, "UTF-8");
 	 */
-
+	/*
+	 * public static void main(String... args) { List<String> lines;
+	 * LinkedList<String> words;
+	 * 
+	 * Map<String, Integer> contagem = new HashMap<String, Integer>();
+	 * 
+	 * // 1) Ler arquivo de entrada lines = readFile();
+	 * 
+	 * // 2) Separar as palavras words = split(lines);
+	 * 
+	 * // 3) Efetuar logica de comparacao contarPalavras(words, contagem);
+	 * 
+	 * // 3.1) colocando ordem no map // Ordenar pelo value, quantidade de usos
+	 * contagem = FileUtil.sortByValue(contagem);
+	 * 
+	 * // Ordenar pela palavra pra gente comparar as coisas // Map<String,
+	 * Integer> treeMap = new TreeMap<String, // Integer>(contagem);
+	 * 
+	 * // 4) Contabilizar ocorrencia das palavras
+	 * 
+	 * for (Entry<String, Integer> key : contagem.entrySet()) {
+	 * System.out.println("" + key.getKey() + " : " + key.getValue()); } }
+	 */
+	
 	public static void main(String... args) {
-		Main m = new Main();
-		List<String> lines;
-		LinkedList<String> words;
+		HTMLsUtils.testeUrl();
+	}
 
-		Map<String, Integer> contagem = new HashMap<String, Integer>();
-
-		/* 1) Ler arquivo de entrada */
-		lines = m.readFile();
-
-		/* 2) Separar as palavras */
-		words = m.split(lines);
-
-		/* 3) Efetuar logica de comparacao */
+	public static void contarPalavras(LinkedList<String> words, Map<String, Integer> contagem) {
 		for (String word : words) {
 			if (contagem.containsKey(word)) {
 				contagem.put(word, contagem.get(word) + 1);
@@ -42,22 +53,9 @@ public class Main {
 				contagem.put(word, 1);
 			}
 		}
-
-		/* 3.1) colocando ordem no map */
-		// Ordenar pelo value, quantidade de usos
-//		contagem = FileUtil.sortByValue(contagem);
-
-		// Ordenar pela palavra pra gente comparar as coisas
-		Map<String, Integer> treeMap = new TreeMap<String, Integer>(contagem);
-
-		/* 4) Contabilizar ocorrencia das palavras */
-
-		for (Entry<String, Integer> key : treeMap.entrySet()) {
-			System.out.println("" + key.getKey() + " : " + key.getValue());
-		}
 	}
 
-	public List<String> readFile() {
+	public static List<String> readFile() {
 		try {
 			File file = new File("D:/Cicero/GDrive/WorkSpace/EclipseProjects/compteur-mots/Arquivos/site_cinema.txt");
 			return FileUtils.readLines(file, "UTF-8");
@@ -67,35 +65,16 @@ public class Main {
 		return null;
 	}
 
-	public LinkedList<String> split(List<String> lines) {
+	public static LinkedList<String> split(List<String> lines) {
 		LinkedList<String> words = new LinkedList<String>();
-
-		// Pattern pattern = Pattern.compile("\\ ");
 
 		for (String currentLine : lines) {
 			StringTokenizer tokenizer = new StringTokenizer(currentLine);
-
 			while (tokenizer.hasMoreTokens()) {
 				// retira pontos, de finais de frases
 				words.add(FileUtil.retirarPontuacao(tokenizer.nextToken()));
 			}
 		}
-
 		return words;
 	}
-
-	/* Exemplo leitura html */
-
-	/*
-	 * InputStream in; try { in = new
-	 * URL("http://commons.apache.org").openStream();
-	 * 
-	 * InputStreamReader inR = new InputStreamReader(in); BufferedReader buf =
-	 * new BufferedReader(inR); String line; while ((line = buf.readLine()) !=
-	 * null) { System.out.println(line); }
-	 * 
-	 * } catch (IOException e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); }
-	 */
-
 }
